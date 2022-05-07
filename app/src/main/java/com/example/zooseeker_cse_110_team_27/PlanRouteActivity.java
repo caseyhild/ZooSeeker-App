4f03d010e5d642ed6095b3929894001860dee0a9
otterice <josephdang101@gmail.com>
2022-05-07 02:42:52 -0700

    Plan route directions with next closest exhibit button

--- app/src/main/java/com/example/zooseeker_cse_110_team_27/PlanRouteActivity.java
+++ app/src/main/java/com/example/zooseeker_cse_110_team_27/PlanRouteActivity.java
@@ -5,16 +5,27 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class PlanRouteActivity extends AppCompatActivity {
-    public RecyclerView rv;
+    private TextView tv;
+    private Button nextBtn;
+    private ArrayList<String> goals;
+    private List<List<String>> shortPaths;
+    private Map<String, ZooData.VertexInfo> vInfo;
+    private Map<String, ZooData.EdgeInfo> eInfo;
+    private Graph<String, IdentifiedWeightedEdge> g;
 
 
     @Override
@@ -22,42 +33,87 @@ public class PlanRouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_route);
 
-        PlanRouteAdapter adapter = new PlanRouteAdapter();
-        adapter.setHasStableIds(true);
-
-        rv = findViewById(R.id.route_items);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.setAdapter(adapter);
-
         // "source" and "sink" are graph terms for the start and end
         String start = "entrance_exit_gate";
-        String goal = "elephant_odyssey";
-        //ArrayList<GraphPath> gpList = new ArrayList<>();
+        goals = new ArrayList<>(Arrays.asList("elephant_odyssey", "gorillas", "lions"));
+
+        tv = findViewById(R.id.directions_textView);
+
+        nextBtn = findViewById(R.id.next_btn);
+        nextBtn.setOnClickListener(this::onNextCicked);
 
         // 1. Load the graph...
-        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
-        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goal);
+        String goal = "";
+        tv.setText("");
+        g = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
+        GraphPath<String, IdentifiedWeightedEdge> shortestPath = null;
+
+        shortPaths = new ArrayList<>();
+
+        while (!goals.isEmpty()) {
+            for (String goale : goals) {
+                GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goale);
+
+                if (shortestPath == null || path.getWeight() < shortestPath.getWeight()) {
+                    shortestPath = path;
+                    goal = goale;
+                }
+            }
+
+            shortestPath = null;
+
+            shortPaths.add(Arrays.asList(start, goal));
+            start = goal;
+            goals.remove(goal);
+        }
+
+        System.out.println(shortPaths.toString());
 
         // 2. Load the information about our nodes and edges...
-        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
-        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON(this,"sample_edge_info.json");
+        vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
+        eInfo = ZooData.loadEdgeInfoJSON(this,"sample_edge_info.json");
 
-        System.out.printf("The shortest path from '%s' to '%s' is:\n", start, goal);
+        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g,
+                shortPaths.get(0).get(0),
+                shortPaths.get(0).get(1));
 
-        System.out.println(path.getWeight());
 
+        tv.append("The shortest path from " + shortPaths.get(0).get(0) + " to " + shortPaths.get(0).get(1)
+                + " is: " + path.getWeight() + " meters.\n\n");
 
         int i = 1;
         for (IdentifiedWeightedEdge e : path.getEdgeList()) {
-            System.out.printf("  %d. Walk %.0f meters along %s from '%s' to '%s'.\n",
-                    i,
-                    g.getEdgeWeight(e),
-                    eInfo.get(e.getId()).street,
-                    vInfo.get(g.getEdgeSource(e).toString()).name,
-                    vInfo.get(g.getEdgeTarget(e).toString()).name);
+
+            tv.append(i + ". Walk " + g.getEdgeWeight(e) + " meters along " + eInfo.get(e.getId()).street +
+                    " from " + vInfo.get(g.getEdgeSource(e).toString()).name + " to " +
+                    vInfo.get(g.getEdgeTarget(e).toString()).name + ".\n\n");
             i++;
         }
 
+        shortPaths.remove(0);
+    }
+
+    private void onNextCicked(View view) {
+        tv.setText("");
+
+        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g,
+                shortPaths.get(0).get(0),
+                shortPaths.get(0).get(1));
+
+
+        tv.append("The shortest path from " + shortPaths.get(0).get(0) + " to " + shortPaths.get(0).get(1)
+                + " is: " + path.getWeight() + " meters.\n\n");
+
+        int i = 1;
+        for (IdentifiedWeightedEdge e : path.getEdgeList()) {
+
+            tv.append(i + ". Walk " + g.getEdgeWeight(e) + " meters along " + eInfo.get(e.getId()).street +
+                    " from " + vInfo.get(g.getEdgeSource(e).toString()).name + " to " +
+                    vInfo.get(g.getEdgeTarget(e).toString()).name + ".\n\n");
+            i++;
+        }
+
+        shortPaths.remove(0);
 
     }
 }
