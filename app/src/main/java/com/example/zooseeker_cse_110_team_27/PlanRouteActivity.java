5226f094025d3ecc1c2ddcf75d79e127541c7745
otterice <josephdang101@gmail.com>
2022-05-22 15:32:43 -0700

    Refactored PlanRouteActivity for SRP and made Compact List

--- app/src/main/java/com/example/zooseeker_cse_110_team_27/PlanRouteActivity.java
+++ app/src/main/java/com/example/zooseeker_cse_110_team_27/PlanRouteActivity.java
@@ -2,7 +2,9 @@ package com.example.zooseeker_cse_110_team_27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,17 +14,18 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-public class PlanRouteActivity extends AppCompatActivity {
+public class PlanRouteActivity extends AppCompatActivity{
     private TextView tv;
     private Button nextBtn;
+    private Button compactBtn;
     private ArrayList<String> goals;
-    private List<List<String>> shortPaths;
-    private Map<String, ZooData.VertexInfo> vInfo;
-    private Map<String, ZooData.EdgeInfo> eInfo;
-    private Graph<String, IdentifiedWeightedEdge> g;
+    private ArrayList<ArrayList<String>> shortPaths;
+
+    private PlanRoute pr;
 
 
     @Override
@@ -39,50 +42,30 @@ public class PlanRouteActivity extends AppCompatActivity {
         nextBtn = findViewById(R.id.next_btn);
         nextBtn.setOnClickListener(this::onNextClicked);
 
-        // 1. Load the graph...
-        String goal = "";
-        //tv.setText("");
-        g = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
-        // 2. Load the information about our nodes and edges...
-        vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
-        eInfo = ZooData.loadEdgeInfoJSON(this,"sample_edge_info.json");
+        compactBtn = findViewById(R.id.compact_btn);
+        compactBtn.setOnClickListener(this::onCompactClicked);
 
         shortPaths = new ArrayList<>();
 
-        PlanRoute.createGoals(g, goals, start, goal, shortPaths);
+        pr = new PlanRoute(this);
+        pr.createGoals(goals, start, shortPaths);
 
         setShortestPath(shortPaths);
     }
 
-    private void setShortestPath(List<List<String>> shortPaths) {
+    private void setShortestPath(ArrayList<ArrayList<String>> shortPaths) {
         tv.setText("");
 
-        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g,
-                shortPaths.get(0).get(0),
-                shortPaths.get(0).get(1));
+        tv.setText(pr.setShortestPath(shortPaths));
 
-        tv.append("The shortest path from " + vInfo.get(path.getStartVertex()).name + " to "
-                + vInfo.get(path.getEndVertex()).name + " is: " + path.getWeight() + " meters.\n\n");
+    }
 
-        int i = 1;
-        String currExhibit = shortPaths.get(0).get(0);
-        for (IdentifiedWeightedEdge e : path.getEdgeList()) {
-            ZooData.VertexInfo edgeSource = vInfo.get(g.getEdgeSource(e));
-            ZooData.VertexInfo edgeTarget = vInfo.get(g.getEdgeTarget(e));
+    private void onCompactClicked(View view) {
+        Intent i = new Intent(this, CompactListActivity.class);
 
-            tv.append(i + ". Walk " + g.getEdgeWeight(e) + " meters along " + eInfo.get(e.getId()).street + " from ");
+        i.putExtra("shortPaths", shortPaths);
 
-            if (currExhibit.equals(edgeSource.id)) {
-                tv.append(edgeSource.name + " to " + edgeTarget.name + ".\n\n");
-                currExhibit = edgeTarget.id;
-            } else {
-                tv.append(edgeTarget.name + " to " + edgeSource.name + ".\n\n");
-                currExhibit = edgeSource.id;
-            }
-            i++;
-        }
-
-        shortPaths.remove(0);
+        startActivity(i);
     }
 
     private void onNextClicked(View view) {
@@ -97,4 +80,5 @@ public class PlanRouteActivity extends AppCompatActivity {
 
         setShortestPath(shortPaths);
     }
+
 }
