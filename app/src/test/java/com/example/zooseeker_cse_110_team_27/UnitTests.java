6760f8311fdd7dae0161cb2d29aa145553eaf9b7
Trevor <trevt.sd@gmail.com>
2022-05-31 21:45:04 -0700

    Unit tests

--- app/src/test/java/com/example/zooseeker_cse_110_team_27/UnitTests.java
+++ app/src/test/java/com/example/zooseeker_cse_110_team_27/UnitTests.java
@@ -9,6 +9,7 @@ import org.robolectric.shadows.ShadowMediaPlayer;
 
 import static org.junit.Assert.*;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,9 @@ import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -35,23 +38,40 @@ import java.util.List;
  */
 @RunWith(AndroidJUnit4.class)
 public class UnitTests {
+    private SearchListItemDao dao;
+    private SearchDatabase db;
+
     @Rule
     public ActivityScenarioRule<SearchListActivity> scenarioRule = new ActivityScenarioRule<>(SearchListActivity.class);
-    public ActivityScenarioRule<CompactListActivity> scenarioRuleCompact = new ActivityScenarioRule<>(CompactListActivity.class);
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context,SearchDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        dao = db.searchListItemDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
 
     @Test
-    public void selectedDisplayed() {
-        ActivityScenario<SearchListActivity> scenario = scenarioRule.getScenario();
+    public void addExhibitToDao() {
+        SearchListItem exhibit1 = new SearchListItem("Gorillas",0,true );
+        SearchListItem exhibit2 = new SearchListItem("Flamingos",1,false );
 
-        scenario.moveToState(Lifecycle.State.CREATED);
+        long id1 = dao.insert(exhibit1);
+        long id2 = dao.insert(exhibit2);
 
-        scenario.onActivity(activity -> {
-            ListView list = activity.findViewById(R.id.list_view);
-            List<SearchListItem> exhibits = activity.returnExhibitList();
+        assertNotEquals(id1,id2);
+        assertEquals(exhibit1.exhibitName,dao.get(id1).exhibitName);
+        assertEquals(exhibit2.exhibitName,dao.get(id2).exhibitName);
+        assertEquals(exhibit1.selected,true);
+        assertEquals(exhibit2.selected,false);
 
-            assertNotNull(exhibits);
-            assertNotNull(list);
-        });
     }
 
 
